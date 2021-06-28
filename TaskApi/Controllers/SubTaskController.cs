6af7623ce3ff4bb124f8d376cb115ffccdedc37a
Jeenal Patel <jspatel1993@gmail.com>
2021-06-27 21:30:12 -0700

    updated all controllers

--- TaskApi/Controllers/SubTaskController.cs
+++ TaskApi/Controllers/SubTaskController.cs
@@ -3,15 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TaskApi.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]/{TaskId}")]
     public class SubTaskController:ControllerBase
     {
         [HttpPost]
-        public async Task<ActionResult> CreateMaterialType(string clothMaterialType)
+        public async Task<ActionResult> CreateMaterialType()
         {
             
         }
+        //make changes using SubTaskId
         [HttpPut("{id:int}")]
-        public async Task<ActionResult>UpdateSubTaskInfo()
+        public async Task<ActionResult>UpdateSubTaskInfo(int id ,TaskModel model)
         {
             
         }
