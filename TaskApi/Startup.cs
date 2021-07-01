b3934cdda115cc082ea9bb12ae86e75fcd457e35
Jeenal Patel <jspatel1993@gmail.com>
2021-06-30 23:04:09 -0700

    Updated Startup.cs and Wrote Post Method in UserController.cs

--- TaskApi/Startup.cs
+++ TaskApi/Startup.cs
@@ -26,7 +26,7 @@ namespace TaskApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TaskContext>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
